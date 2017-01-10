1fa076cc55f45c271e55259e51e053d8a28e1f67
Marvin Vilaysack <vilay006@umn.edu>
2017-01-09 18:52:38 -0600

    Update dm.php

--- dm.php
+++ dm.php
@@ -5,28 +5,28 @@ if ($_GET['user']) // ?user=channelname
 	$user = $_GET['user'];
 	$token = ''; //insert token -- can never be permanant because of DailyMotion's token expiration
 	$array = file_get_contents("https://api.dailymotion.com/user/{$user}/videos?access_token=" . $token . "&limit=100"); // Defining the JSON, the limit is 100 per page.
-	$rawr = json_decode($array);
-	foreach ( $rawr->list as $list )
+	$newList = json_decode($array);
+	foreach ( $newList->list as $list )
 	{
 		echo "http://www.dailymotion.com/video/{$list->id}<br />";
 	}
 	$page = 1;
-	if ($rawr->has_more == "true")
+	if ($$newList->has_more == "true")
 	{
 		//checks if it has more pages TODO: Actually make a loop, at the most, this will grab only 300 videos
 		$page++;
 		$arrayz = file_get_contents("https://api.dailymotion.com/user/{$user}/videos?access_token=" . $token . "&limit=100&page={$page}"); // Defining the JSON, the limit is 100 per page.
-		$rawrz = json_decode($arrayz);
-		foreach ( $rawrz->list as $list )
+		$newList1 = json_decode($arrayz);
+		foreach ( $newList1->list as $list )
 		{
 			echo "http://www.dailymotion.com/video/{$list->id}<br />";
 		}
-		if ($rawrz->has_more == "true")
+		if ($newList->has_more == "true")
 		{
 			$page++;
 			$arrayzz = file_get_contents("https://api.dailymotion.com/user/{$user}/videos?access_token=" . $token  . "&limit=100&page={$page}"); // Defining the JSON, the limit is 100 per page.
-			$rawrzz = json_decode($arrayzz);
-			foreach ( $rawrzz->list as $list )
+			$newList2 = json_decode($arrayzz);
+			foreach ( $newList2->list as $list )
 			{
 				echo "http://www.dailymotion.com/video/{$list->id}<br />";
 			}
